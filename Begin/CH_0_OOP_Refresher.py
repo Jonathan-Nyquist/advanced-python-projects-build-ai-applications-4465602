96a3b69469655d375edfb12fcb8003efe4cad996
Jonathan Nyquist <103674665+Jonathan-Nyquist@users.noreply.github.com>
2025-03-17 20:10:00 +0000

    Created simple class for 1st tutorial

--- Begin/CH_0_OOP_Refresher.py
+++ Begin/CH_0_OOP_Refresher.py
@@ -1,35 +1,28 @@
 # Introduction to Object-Oriented Programming with Python: Creating and Using Classes
 # My change
 # Class Definition
-
+class Car:
 # Constructor (Initialization) - __init__ method
-
-# Encapsulation: Attributes (make and model) are encapsulated within the class.
-
-
-# Method - start_engine
-
-# Encapsulation: Accessing attributes through self.
-
-
+  def __init__(self, make, model):
+    # Encapsulation: Attributes (make and model) are encapsulated within the class.
+    self.make = make
+    self.model = model
+    
+  # Method - start_engine  
+  def start_engine(self):
+    print(f"The {self.make} {self.model}'s engine is running!")
+    
 # Creating instances (objects) of the Car class
-
 # Inheritance: Car is a class that can be used to create objects (instances).
 # Abstraction: We create objects without worrying about the internal details of the Car class.
+car1 = Car("Toyota", "Camry")
+car2 = Car("Ford", "Mustang")
 
-# Creating the first car (object)
-
-# Creating the second car (object)
-
-
-# Accessing object attributes
-
-# Encapsulation: Accessing object attributes (make and model) using dot notation.
-
-
-# Calling object methods
+print(f"I have a {car1.make} {car1.model}.")
+print(f"I also own a {car2.make} {car2.model}.")
 
 # Polymorphism: Different objects (car1 and car2) can perform the same action (start_engine).
-
+car1.start_engine()
+car2.start_engine()
 
 # Method Call - start_engine
